af2a96e626c1fb05cb5765950acd4f49da79e710
Jean Laneurit <jean.laneurit@irstea.fr>
2018-01-05 09:08:30 +0100

    [wgs84 path matching] fix compilation bug under ubuntu 14.04

--- apps/wgs84_path_matching/src/wgs84_path_matching_system.hpp
+++ apps/wgs84_path_matching/src/wgs84_path_matching_system.hpp
@@ -34,8 +34,8 @@ protected:
 
   void publishTf_(const ros::TimerEvent & event);
 
-//  void displayResults(const romea::Duration &duration,
-//                      const std::vector<romea::Range::Ptr> & ranges);
+  //  void displayResults(const romea::Duration &duration,
+  //                      const std::vector<romea::Range::Ptr> & ranges);
 
 protected:
 
@@ -57,9 +57,13 @@ protected:
 
   bool display_;
   rviz_visual_tools::RvizVisualTools rviz_util_;
+#if ROS_VERSION_MINIMUM(1,12,0)
   romea::VectorOfEigenVector<Eigen::Vector3d> path3d_;
   romea::VectorOfEigenVector<Eigen::Vector3d> interpolatedPath3d_;
-
+#else
+  std::vector<Eigen::Vector3d> path3d_;
+  std::vector<Eigen::Vector3d> interpolatedPath3d_;
+#endif
 
   WGS84PathMatchingDiagnostic diagnostics_;
 

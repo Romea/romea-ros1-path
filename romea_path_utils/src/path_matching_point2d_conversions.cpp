cec1094b2ac028a13c035c0099ee2a2be7f2e8e5
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-04-23 21:29:45 +0200

    Add section_maximal_curvilinear_abscissa

--- romea_path_utils/src/path_matching_point2d_conversions.cpp
+++ romea_path_utils/src/path_matching_point2d_conversions.cpp
@@ -14,6 +14,10 @@ void to_ros_msg(const core::PathMatchedPoint2D & romea_matched_point2d,
   ros_path_matched_point2d_msg.future_curvature= romea_matched_point2d.futureCurvature;
   ros_path_matched_point2d_msg.desired_speed= romea_matched_point2d.desiredSpeed;
   ros_path_matched_point2d_msg.section_index = romea_matched_point2d.sectionIndex;
+  ros_path_matched_point2d_msg.section_minimal_curvilinear_abscissa =
+    romea_matched_point2d.sectionMinimalCurvilinearAbscissa;
+  ros_path_matched_point2d_msg.section_maximal_curvilinear_abscissa =
+    romea_matched_point2d.sectionMaximalCurvilinearAbscissa;
   ros_path_matched_point2d_msg.curve_index = romea_matched_point2d.curveIndex;
 }
 
@@ -26,27 +30,13 @@ void to_romea(const romea_path_msgs::PathMatchedPoint2D &matched_point_msg,
   to_romea(matched_point_msg.frenet_pose,romea_matched_point.frenetPose);
   romea_matched_point.desiredSpeed=matched_point_msg.desired_speed;
   romea_matched_point.sectionIndex=matched_point_msg.section_index;
+  romea_matched_point.sectionMinimalCurvilinearAbscissa =
+    matched_point_msg.section_minimal_curvilinear_abscissa;
+  romea_matched_point.sectionMaximalCurvilinearAbscissa =
+    matched_point_msg.section_maximal_curvilinear_abscissa;
   romea_matched_point.curveIndex=matched_point_msg.curve_index;
 }
 
-
-////-----------------------------------------------------------------------------
-//void to_ros_msg(const PathMatchedPoint2D & romea_matched_point2d,
-//              romea_path_msgs::PathMatchedPoint2D & ros_path_matched_point2d_msg)
-//{
-//  to_ros_msg(romea_matched_point2d.pathPosture,ros_path_matched_point2d_msg.posture);
-//  to_ros_msg(romea_matched_point2d.frenetPose,ros_path_matched_point2d_msg.frenet_pose);
-//  ros_path_matched_point2d_msg.nearest_point_index = romea_matched_point2d.nearestPointIndex;
-//}
-
-////-----------------------------------------------------------------------------
-//void to_romea(const romea_path_msgs::PathMatchedPoint2D &matched_point_msg,
-//             PathMatchedPoint2D & romea_matched_point)
-//{
-//  to_romea(matched_point_msg.posture,romea_matched_point.pathPosture);
-//  to_romea(matched_point_msg.frenet_pose,romea_matched_point.frenetPose);
-//  romea_matched_point.nearestPointIndex=matched_point_msg.nearest_point_index;
-//}
 
 //-----------------------------------------------------------------------------
 core::PathMatchedPoint2D to_romea(const romea_path_msgs::PathMatchedPoint2D &matched_point_msg)
